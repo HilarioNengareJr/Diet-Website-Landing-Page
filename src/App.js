846ccddb603bf93709090e6970ce75aa22109b5d
Hilario Nengare <hnengare@gmail.com>
2023-09-19 17:55:03 +0300

    sdfdfd

--- src/App.js
+++ src/App.js
@@ -7,47 +7,21 @@ import Classes from './components/Classes';
 import Blog from './components/Blog';
 import Footer from './components/Footer';
 import AppCard from './components/AppCard';
-import BackTop from './components/Backtop';
-import Fade from 'react-reveal/Fade';
-import Slide from 'react-reveal/Slide';
 import './App.css';
-
+import BackTop from './components/BackTop';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
-
-        <Fade top>
-          <Hero />
-        </Fade>
-        
-        <Fade left>
-          <Service />
-        </Fade>
-        
-        <Fade right>
-          <About />
-        </Fade>
-        
-        <Slide left>
-          <Classes />
-        </Slide>
-        
-        <Slide right>
-          <Blog />
-        </Slide>
-        
-        <Fade left>
-          <AppCard />
-        </Fade>
-        
-     
-        <Fade bottom>
-          <Footer />
-        </Fade>
-        
+        <Hero />
+        <Service />
+        <About />
+        <Classes />
+        <Blog />
+        <AppCard />
+        <Footer />
         <BackTop />
       </React.Fragment>
     );
